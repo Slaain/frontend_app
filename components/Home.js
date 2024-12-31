5bf194f5c11bfac944f30b965a0d53ac699a7db1
Ali <idris1390@gmail.com>
2024-12-31 19:18:55 +0100

    installation tailwind

--- components/Home.js
+++ components/Home.js
@@ -1,15 +1,11 @@
 import styles from '../styles/Home.module.css';
 
 function Home() {
-  return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
-    </div>
-  );
+    return (
+        <div className="flex items-center justify-center h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold text-red-800">Bienvenue sur TeamBoard !</h1>
+        </div>
+    );
 }
 
 export default Home;
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,51 @@
 import '../styles/globals.css';
 import Head from 'next/head';
+import { Provider } from 'react-redux';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+
+// Exemple de reducers (à remplacer par vos reducers réels)
+const userReducer = (state = {}, action) => state; // Remplacez par votre reducer
+const projectReducer = (state = {}, action) => state; // Remplacez par votre reducer
+
+// Combine reducers
+const reducers = combineReducers({
+    user: userReducer,
+    project: projectReducer,
+});
+
+// Configurer redux-persist
+const persistConfig = {
+    key: 'teamboard',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+// Configurer le store Redux
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false, // Nécessaire pour redux-persist
+        }),
+});
+
+const persistor = persistStore(store);
 
 function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>Next.js App</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+    return (
+        <Provider store={store}>
+            <PersistGate persistor={persistor}>
+                <Head>
+                    <title>TeamBoard</title>
+                </Head>
+                <Component {...pageProps} />
+            </PersistGate>
+        </Provider>
+    );
 }
 
 export default App;
